0c78e740d5641acbd19f83d8492ee0af4cd5fa86
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-01-13 21:14:35 +0530

    completed Message component with interactive reactions and record display

--- src/component/MessageInput.jsx
+++ src/component/MessageInput.jsx
@@ -144,3 +144,164 @@ function Message({ messageText, messageTime, messageId }) {
             }
         }
     });
+
+    return (
+        <React.Fragment>
+            <div className="message-component-wrapper h-fit flex flex-col items-end justify-end">
+                <div className="reaction-record-list-overlay-wrapper flex-col items-start gap-1 max-h-[240px] h-fit w-[320px] pb-[20px] bg-zinc-900 shadow-2xl rounded-lg overflow-x-hidden overflow-y-scroll"
+                    style={{
+                        display: moreReactionsListVisibility
+                    }}
+                >
+                    <div className="h-fit w-full flex flex-row items-center justify-start border-b border-gray-800 px-4 py-4">
+                        <FaArrowLeft className="text-white" />
+                    </div>
+                    <div className="h-fit w-full px-4 py-3">
+                        <span className="leading-snug text-gray-600 text-xs font-semibold">More Reactions</span>
+                    </div>
+                    <div className="more-emojis-reaction-list-wrapper grid grid-cols-6 items-center justify-between">
+                        {moreReactionsListRef?.map((moreReactionEmoji, moreReactionEmojiIndex) => {
+                            return (
+                                <button key={moreReactionEmojiIndex}
+                                    className="p-2 rounded-full bg-transparent hover:bg-white hover:bg-opacity-20"
+                                >
+                                    {moreReactionEmoji?.image}
+                                </button>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="message-reactions-list-option-wrapper w-[280px] h-fit flex flex-row items-center justify-center gap-1 bg-zinc-900 rounded-full px-3 py-1 shadow-lg transition-all"
+                    style={{
+                        display: reactionListVisibility
+                    }}
+                >
+                    {reactionsList?.map((reaction, reactionIndex) => {
+                        return (
+                            <button className="p-2 rounded-full bg-transparent hover:bg-white hover:bg-opacity-20"
+                                key={reactionIndex}
+                                onClick={() => {
+                                    let _addedReactionsList = addedReactionsList;
+                                    reactionRecord?.map((_reaction) => {
+                                        _addedReactionsList.push(_reaction?.reactionName);
+                                        setAddedReactionsList(_addedReactionsList);
+                                        return <React.Fragment></React.Fragment>
+                                    });
+                                    if (!addedReactionsList.includes(reaction?.name)) {
+                                        let _newReactionRecord = reactionRecord;
+                                        _newReactionRecord?.push({
+                                            emoji: getReactionData(reaction?.name),
+                                            profile: {
+                                                username: 'You',
+                                                pictureURL: getProfilePicture(),
+                                                picture: <img 
+                                                            src={getProfilePicture()} 
+                                                            alt="profile"
+                                                            className="profile-picture rounded-full w-10 h-10"
+                                                        />
+                                            },
+                                            reactionName: reaction?.name
+                                        });
+                                        setReactionRecord(_newReactionRecord);
+                                    }
+                                }}
+                            >
+                                <img 
+                                    src={reaction?.content} 
+                                    alt='emoji-reaction' 
+                                />
+                            </button>
+                        )
+                    })}
+                    <button className="p-2 text-white rounded-full bg-transparent hover:bg-white hover:bg-opacity-20"
+                        onClick={() => {
+                            setReactionListVisibility('none');
+                            setMoreReactionsListVisibility('flex');
+                        }}
+                    >
+                        <FaPlusCircle />
+                    </button>
+                </div>
+                <div className="message-block-reaction_action-wrapper flex flex-row items-center justify-end gap-2"
+                    onMouseEnter={() => setReactionActionVisibility('60%')}
+                    onMouseLeave={() => setReactionActionVisibility('0%')}
+                    onClick={() => setReactionListVisibility('flex')}
+                >
+                    <button className="message-reaction-action-icon-wrapper w-fit h-fit flex flex-row items-center cursor-pointer justify-center text-white text-opacity-60 p-[4px] bg-white bg-opacity-40 rounded-full"
+                        id={messageId}
+                        style={{
+                            opacity: reactionActionVisibilty
+                        }}
+                        onClick={() => {
+                            if (reactionRecordListWrapperVisibility.toLowerCase() === 'flex') {
+                                setReactionRecordListWrapperVisibility('none')
+                            }
+                        }}
+                    >
+                        <FaSmile />
+                    </button>
+                    <div className="message-block-wrapper flex flex-row items-end gap-3 my-3 rounded-md justify-start w-fit px-3 py-1 bg-teal-700">
+                        <span className="message-text-wrapper font-semibold text-sm text-white">{messageText}</span>
+                        <span className="message-time-wrapper font-normal text-xs text-white text-opacity-60">
+                            {messageTime?.hours}:{messageTime?.mins}{" "}{messageTime?.mrdn}
+                        </span>
+                    </div>
+                </div>
+                <div className="reaction-record-list-action-button-wrapper">
+                    <div className="reaction-record-list-overlay-wrapper flex-col items-start gap-1 h-fit w-[320px] pb-[20px] bg-zinc-900 shadow-2xl rounded-lg"
+                        style={{
+                            display: reactionRecordListWrapperVisibility
+                        }}
+                    >
+                        <div className="reaction-record-list-columns-wrapper px-4 py-3">
+                            <button className="all-reactions-tab-button bg-transparent text-white text-sm flex flex-row items-center justify-center gap-1
+                                hover:bg-white hover:bg-opacity-20 rounded-md px-3 py-1
+                            ">
+                                <span className="font-semibold">All</span> 
+                                <span className="font-normal text-zinc-400">{reactionRecord?.length}</span>
+                            </button>
+                        </div>
+                        {reactionRecord?.map((_reaction, _reactionIndex) => {
+                            return (
+                                <React.Fragment key={_reactionIndex}>
+                                    <div className="reaction-record-item-wrapper h-fit w-full bg-transparent hover:bg-white hover:bg-opacity-20
+                                        flex flex-row items-center justify-between px-4 py-2
+                                    ">
+                                        <div className="profile-name-picture-record-wrapper flex flex-row items-center justify-start gap-2">
+                                            <span className="profile-picture-wrapper">{_reaction?.profile?.picture}</span>
+                                            <span className="username-wrapper text-white font-semibold text-sm leading-snug">{_reaction?.profile?.username}</span>
+                                        </div>
+                                        <span className="reaction-emoji-wrapper">
+                                            <img src={_reaction?.emoji} alt="emoji-record" 
+                                                style={{ width: '20px', height: '20px' }}
+                                            />
+                                        </span>
+                                    </div>
+                                </React.Fragment>
+                            )
+                        })}
+                    </div>
+                    {reactionRecord && reactionRecord.length > 0
+                        ? <button className="reaction-record-list-action-button px-[4px] py-[2px] bg-teal-600 rounded-full flex flex-row items-center justify-center gap-[1.5px] -mt-1 mb-1.5"
+                            onClick={() => setReactionRecordListWrapperVisibility('flex')}
+                            id={`message-record-list-opener_${messageId}`}
+                        >
+                            {reactionRecord?.map((reactionEmoji, reactionEmojiIndex) => {
+                                return (
+                                    <img src={reactionEmoji?.emoji} alt="emoji-record" key={reactionEmojiIndex} 
+                                        style={{ width: '14px', height: '14px' }}
+                                    />
+                                )
+                            })}
+                            {reactionRecord.length > 1
+                                ? <span className="text-xs text-white text-opacity-50 font-normal">{reactionRecord.length}</span>
+                                : <React.Fragment></React.Fragment>
+                            }
+                        </button>
+                        : <React.Fragment></React.Fragment>
+                    }
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
